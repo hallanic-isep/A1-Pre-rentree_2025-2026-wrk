a47858be4ec44512d33f924ccd513023d6e3eb7d
toto <hallanic-isep>
2025-08-26 10:06:58 +0200

    Ajout boucle 'for'

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import random
 prix_a_trouver = random.randint(1,10)
 print(f"Prix à trouver : {prix_a_trouver}")
 
-# L'essai
-essai = input("Votre proposition (entre 1 et 10) : ")
-essai = int(essai)
+for i in range(1, 6):
 
-#print(f"Type de prix_a_trouver : {type(prix_a_trouver)}")
-#print(f"Type de essai : {type(essai)}")
+    # L'essai
+    essai = input(f"Essai n°{i} : Votre proposition (entre 1 et 10) : ")
+    essai = int(essai)
 
-# La comparaison
-if prix_a_trouver == essai :
-    print("Bravo !")
-elif prix_a_trouver < essai :
-    print("Trop élévé...")
-else:
-    print("Trop peu...")
+    #print(f"Type de prix_a_trouver : {type(prix_a_trouver)}")
+    #print(f"Type de essai : {type(essai)}")
+
+    # La comparaison
+    if prix_a_trouver == essai :
+        print("Bravo !")
+        break
+    elif prix_a_trouver < essai :
+        print("Trop élévé...")
+    else:
+        print("Trop peu...")
+
+print("Fin...")
